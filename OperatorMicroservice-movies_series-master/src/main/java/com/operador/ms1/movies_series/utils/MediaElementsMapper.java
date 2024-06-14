51783788d246742466cb7e4d310cf4c3977adc1e
Oriel Camaño <oricam28@hotmail.com>
2024-06-14 15:53:19 -0500

    actualizacion de DB y de mapper

--- OperatorMicroservice-movies_series-master/src/main/java/com/operador/ms1/movies_series/utils/MediaElementsMapper.java
+++ OperatorMicroservice-movies_series-master/src/main/java/com/operador/ms1/movies_series/utils/MediaElementsMapper.java
@@ -6,33 +6,9 @@ public class MediaElementsMapper {
 
     public static MediaModel toUpdate(MediaModel request, MediaModel mediaElement) {
 
-//        if (null != request.getType()) {
-//            mediaElement.setType(request.getType());
-//        }
-//        if (null != request.getTitle()) {
-//            mediaElement.setTitle(request.getTitle());
-//        }
-//        if (null != request.getPoster()) {
-//            mediaElement.setPoster(request.getPoster());
-//        }
-//        if (null != request.getDescription()) {
-//            mediaElement.setDescription(request.getDescription());
-//        }
-//        if (null != request.getDirector()) {
-//            mediaElement.setDirector(request.getDirector());
-//        }
-//        if (null != request.getReleaseYear()) {
-//            mediaElement.setReleaseYear(request.getReleaseYear());
-//        }
-//        if (null != request.getDuration()) {
-//            mediaElement.setDuration(request.getDuration());
-//        }
-//        if (null != request.getRating()) {
-//            mediaElement.setRating(request.getRating());
-//        }
-//        if (null != request.getTrailerID()) {
-//            mediaElement.setTrailerID(request.getTrailerID());
-//        }
+        if (null != request.getMediaElements()) {
+            mediaElement.setMediaElements(request.getMediaElements());
+        }
 
         return mediaElement;
     }
